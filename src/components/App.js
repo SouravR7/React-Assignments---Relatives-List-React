e3f569cfeb674a154b75b1129379f7cb99147ea5
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-11-05 19:43:01 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+
+        return(
+            <div id="main">
+               {/* Do not remove the main div */}
+            </div>
+        )
+    }
+}
+
+
+export default App;
